f8053a767a30500eed74a6b0541201a6d639f689
adnanarsalan <adnanarsalan66@gmail.com>
2022-04-05 13:42:08 +0600

    api

--- script.js
+++ script.js
@@ -1,53 +1,57 @@
-const searchPhone = () => {
-    
-  let searchField = document.getElementById("search-field")
-  let searchText = searchField.value
-  
-  // searchField.value = '' ;
-  let url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-  const searchResult=document.getElementById('search-result')
-  fetch(url)
-  .then(res => res.json())
-  .then(anObject =>{let allDiv = anObject.data.map(item=>`<div class="col-md-4 mb-3">
-  <div class="card h-100">
-    <img src=${item.image} class="card-img-top img-fluid" alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title">${item.phone_name}</h5>
-      <h5 class="card-title">${item.brand}</h5>
-    </div>
-    <div class="card-footer">
-    <button  class="btn btn-info" onclick="showDetails(${item.slug})" >Phone details </button>
-    </div>
-  </div>
-</div> `)
  
-  searchResult.innerHTML=allDiv.join('')
-})}
+ 
+ 
+   function pic ()
+   {
+  
+        let isha = document.getElementById("img02")
+       isha.src="image/dog01.jpg"
+   }
+   function pic2 ()
+   {
+  
+        let isha = document.getElementById("img02")
+       isha.src="image/dog.jpg"
+   }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ function loadData(){
+   fetch('https://api.TheDogAPI.com/v1/breeds')
+   .then(res=>res.json())
+   .then(data=>displayDog(data));
+ }
 
-const showDetails = (id) =>{
-  console.log(id)
-  let searchDeatails = document.getElementById("details")
+ const displayDog = (dogList) => {const main = document.getElementById('main');
+ const first10Data = dogList.slice(0, 15);
+ for(const dog of first10Data) {
 
-  let url = `https://openapi.programming-hero.com/api/phone/${id}`;
-  fetch(url)
-  .then(res => res.json())
-  .then(anObject =>{let details = `<div class="col-md-3">
+ 
+   const div = document.createElement("div")
+   div.className="col-lg-4"
+   div.innerHTML = `
+   
+       <h2 class="dogColor"> ${dog.name}  </h2>
+       <p class="dogColor01">${dog.temperament}  </p>
+       <h4> ${dog.weight.imperial} </h4>
+       <img height="400px" width="400px" src="${dog.image.url}" >
 
-  </div>
-  <div class="col-md-6">
-  <div class="card h-100">
-  <img src=${anObject.data.mainFeatures.image} class="card-img-top img-fluid" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-  </div>
-  <div class="card-footer">
-  <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-  </div>
-  <div class="col-md-3">
-    
-  </div>`
-  searchDeatails.innerHTML= details
-})}
+   
+   
+   `
+    main.appendChild(div)
+    console.log(div)
+ }
+
+
+
+
+
+
+}
+   
